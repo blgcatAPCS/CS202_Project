ffa2989c68234f3680060afbb4b7641f9baea370
tam11112002 <53972061+trannguyendangtam1111@users.noreply.github.com>
2021-11-27 08:25:39 +0700

    cTraffic

--- ctraffic.cpp
+++ ctraffic.cpp
@@ -1,1 +1,43 @@
 #include "header/header.h"
+
+cTraffic::cTraffic()
+{
+	m_status = false;
+	m_time = 0;
+}
+void cTraffic::set(bool status, int time)
+{
+	m_status = status;
+	m_time = time;
+}
+void cTraffic::updateTime()
+{
+	m_time++;
+	if (m_time == MAX_TIME)
+	{
+		this->changeStatus();
+		if (m_status == true)
+		{
+			m_time = 8;
+		}
+		else
+		{
+			m_time = 0;
+		}
+	}
+}
+int cTraffic::getTime()
+{
+	return m_time;
+}
+bool cTraffic::getStatus()
+{
+	return m_status;
+}
+void cTraffic::changeStatus()
+{
+	if (m_status == true)
+		m_status = false;
+	else
+		m_status = true;
+}
--- header/ctraffic.h
+++ header/ctraffic.h
@@ -1,5 +1,20 @@
 #ifndef ctraffich
 #define ctraffich
 #include "header.h"
+#define MAX_TIME 15
+
+class cTraffic
+{
+private:
+	bool m_status; //red light when true
+	int m_time;
+public:
+	cTraffic();
+	bool getStatus();
+	int getTime();
+	void set(bool status, int time);
+	void changeStatus();
+	void updateTime();
+};
 
 #endif
